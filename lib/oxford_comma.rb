736fac20171fb82c010fe2cc50297afab1960214
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-15 01:02:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,2 +1,16 @@
 def oxford_comma(array)
-  
+  def oxford_comma(array)
+  array = [*array] 
+  case array.size
+  when 0
+    ''
+  when 1
+    array[0].to_s
+  when 2
+    array.join(' and ')
+  else
+    array_copy = array
+    array_copy[-1] = "and #{array_copy[-1]}"
+    array_copy.join(', ')
+  end
+end
